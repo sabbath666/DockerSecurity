20d1e4c6c9e5866b19d57e10a63cbab18325cd6a
slava_ivanov <tristandtm@gmail.com>
2025-05-06 15:27:13 +0300

    added log4shell reverse shell example for cowsay

--- cowsay-log4shell/log4shell-rce/src/main/kotlin/ru/cib/log4shellrce/Log4ShellApplication.kt
+++ cowsay-log4shell/log4shell-rce/src/main/kotlin/ru/cib/log4shellrce/Log4ShellApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Log4shellRceApplication
+class Log4ShellApplication
 
 fun main(args: Array<String>) {
-    runApplication<Log4shellRceApplication>(*args)
+    runApplication<Log4ShellApplication>(*args)
 }
--- cowsay-log4shell/log4shell-rce/src/main/kotlin/ru/cib/log4shellrce/Log4jController.kt
+++ cowsay-log4shell/log4shell-rce/src/main/kotlin/ru/cib/log4shellrce/Log4jController.kt
@@ -1,10 +1,14 @@
 package ru.cib.log4shellrce
 
-import org.apache.logging.log4j.LogManager
+import com.github.ricksbrown.cowsay.plugin.CowExecutor
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestHeader
+import java.net.InetAddress
+import java.util.concurrent.atomic.AtomicReference
+import org.apache.logging.log4j.LogManager
 
 @RestController
 class Log4jController {
@@ -13,9 +17,89 @@ class Log4jController {
         private val logger = LogManager.getLogger(Log4jController::class.java)
     }
 
-    @GetMapping("/log")
-    fun logInput(@RequestHeader("X-Api-Version") apiVersion: String): String {
+    private val greetings = listOf("Ну привет", "Чокак?", "Хэй", "Пуньк-пунь")
+    private val monster = AtomicReference("default")
+    private val cowSayExecutor = CowExecutor().apply {
+        setHtml(true)
+    }
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        val greeting = greetings.random()
+        return cowsay(greeting, monster.get())
+    }
+
+    @GetMapping("/sysinfo")
+    fun sysinfo(): String {
+        val ip = getIpAddress()
+        val inContainer = isRunningInContainer()
+        val runtime = Runtime.getRuntime()
+        val cpuCores = runtime.availableProcessors()
+        val maxMemoryMb = runtime.maxMemory() / (1024 * 1024)
+        val allocatedMemoryMb = runtime.totalMemory() / (1024 * 1024)
+        val freeMemoryMb = runtime.freeMemory() / (1024 * 1024)
+        val info = """
+            <pre>
+            Hostname     : ${InetAddress.getLocalHost().hostName}
+            IP           : ${ip}
+            OS Name      : ${System.getProperty("os.name")}
+            OS Version   : ${System.getProperty("os.version")}
+            Kernel       : ${System.getProperty("os.arch")}
+            User         : ${System.getProperty("user.name")}
+            Processes    : ${getProcessCount()}
+            CPU Cores    : $cpuCores
+            Max Memory   : ${maxMemoryMb} MB
+            --------------------------------------------------
+                                          \
+                                           \   ^__^
+                                            \  (oo)\_______
+                                               (__)\       )\/\
+                                                    ||----w |
+                                                    ||     ||
+           </pre>                                         
+        """.trimIndent()
+
+        return info
+    }
+
+    @PostMapping("/setmonster")
+    fun setMonster(@RequestBody request: MonsterRequest,
+                   @RequestHeader("X-Api-Version") apiVersion: String
+    ): String {
         logger.info(apiVersion)
-        return "Logged: $apiVersion"
+        monster.set(request.name)
+        return cowsay("Монстр установлен на '${request.name}'", monster.get())
+    }
+
+    private fun getProcessCount(): Int {
+        return try {
+            ProcessHandle.allProcesses().count().toInt()
+        } catch (e: Exception) {
+            -1
+        }
+    }
+
+    private fun cowsay(message: String, cowfile: String): String {
+        cowSayExecutor.setMessage(message)
+        cowSayExecutor.setCowfile(cowfile)
+        return cowSayExecutor.execute()
+    }
+    private fun getIpAddress(): String {
+        return try {
+            InetAddress.getLocalHost().hostAddress
+        } catch (e: Exception) {
+            "Unknown"
+        }
+    }
+
+    private fun isRunningInContainer(): String {
+        return try {
+            val cgroup = java.io.File("/proc/1/cgroup")
+            if (cgroup.exists() && cgroup.readText().contains("docker")) "Yes" else "No"
+        } catch (e: Exception) {
+            "Unknown"
+        }
     }
 }
+
+data class MonsterRequest(val name: String)
